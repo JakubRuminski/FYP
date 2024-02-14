55ca3219392db2a79dc5ad30b038fd14d34bb9d2
Jakub Ruminski <jakubruminski09@gmail.com>
2024-02-14 08:40:03 +0000

    main.go: + Places clear terminal output command above INIT's

--- main.go
+++ main.go
@@ -12,15 +12,15 @@ import (
 
 
 func main() {
+	fmt.Println("\033[H\033[2J")
 	logger := &logger.Logger{}
 
 	port, mux, ok := mux.INIT(logger)
 	if !ok { fmt.Println("Failed to initialize router"); return }
-
+	
     ok = query.INITIALISE_DATABASE(logger)
 	if !ok { fmt.Println("Failed to initialize database"); return }
-
-	fmt.Println("\033[H\033[2J")
+	
 	fmt.Println("Listening on port http://localhost:" + port)
 
 	http.ListenAndServe(":"+port, mux)
