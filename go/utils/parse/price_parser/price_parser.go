0fd4a900927298ff1c005a28200db06f528d8526
Jakub Ruminski <jakubruminski09@gmail.com>
2024-02-21 07:49:35 +0000

    price_parser.go: + Added more print statements. + Added indexing to print statements. + Corrected mistake of `Float` stripping non numeric characters because this was hidding issue. Instead only dangling blank spaces are removed. + Lines 97 to 102 - Cover the case where the ppu is "€3.50 each" as a posed to "€3.50 per litre" + Line 140 - Fixed major bug where cl or ml or g were being multiplied rather than divided.

--- go/utils/parse/price_parser/price_parser.go
+++ go/utils/parse/price_parser/price_parser.go
@@ -14,15 +14,15 @@ import (
 func Float(index int, logger *logger.Logger, price string) (currency string, priceFloat float64, ok bool) {
 	currency, price, ok = findAndStripCurrency(index, logger, price)
 	if !ok {
-		logger.DEBUG_WARN("Failed to find and strip currency from '%s'", price)
+		logger.DEBUG_WARN("%v - Failed to find and strip currency from '%s'", index, price)
 		return "", 0.0, false
 	}
 
-	price = parse.StripNonNumeric(price)
+	price = strings.Replace(price, " ", "", -1)
 
 	priceFloat, ok = convertToFloat(index, logger, price)
 	if !ok {
-		logger.DEBUG_WARN("Failed to convert string '%s' to float", price)
+		logger.DEBUG_WARN("%v - Failed to convert string '%s' to float", index, price)
 		return "", 0.0, false
 	}
 	
@@ -33,22 +33,20 @@ func Float(index int, logger *logger.Logger, price string) (currency string, pri
 func FloatPerUnit(index int, logger *logger.Logger, price string) (currency string, priceFloat float64, pricePerUnit string, ok bool) {
 	currency, price, ok = findAndStripCurrency(index, logger, price)
 	if !ok {
-		logger.DEBUG_WARN("Failed to find and strip currency from '%s'", price)
+		logger.DEBUG_WARN("%v - Failed to find and strip currency from '%s'", index, price)
 		return "", 0.0, "", false
 	}
 
 	parsedPrice, perUnitQuantity, perUnit, measurement, ok := stripMeasurement(index, logger, price)
 	if !ok {
-		logger.DEBUG_WARN("Failed to strip price '%s'", price)
+		logger.DEBUG_WARN("%v - Failed to strip price '%s'", index, price)
 		return currency, 0.0, "", false
 	}
-	logger.INFO("parsedPrice: %s, perUnitQuantity: %s, perUnit: %s, measurement: %s", parsedPrice, perUnitQuantity, perUnit, measurement)
 	priceFloat, ok = convertPrice(index, logger, parsedPrice, perUnitQuantity, perUnit)
 	if !ok {
-		logger.DEBUG_WARN("Failed to convert price '%s'", price)
+		logger.DEBUG_WARN("%v - Failed to convert price '%s'", index, price)
 		return currency, 0.0, "", false
-	}
-	
+	}	
 	
 	return currency, priceFloat, measurement, true
 }
@@ -64,14 +62,16 @@ func findAndStripCurrency(index int, logger *logger.Logger, price string) (curre
 func convertToFloat(index int, logger *logger.Logger, price string) (priceFloat float64, ok bool) {
 	priceFloat, err := strconv.ParseFloat(price, 64)
 	if err != nil {
-		logger.DEBUG_WARN("Error converting string '%s' to float", price)
+		logger.DEBUG_WARN("%v - Error converting string '%s' to float", index, price)
 		return 0.0, false
 	}
 	
 	return priceFloat, true
 }
 
-
+// Good To Know: The order of the unit types in the map is ignored when iterated over.
+// Sometimescausing kilograms to be stepped over first of grams, and vice versa.
+//
 var unitTypes_DICT = map[string]map[string][]string{
     "kilogram": {
         "kilogram": {"kilograms", "kilogram", "kilo", "kg"},
@@ -94,15 +94,23 @@ func stripMeasurement(index int, logger *logger.Logger, price string) (parsedPri
 		parsedPriceArray = strings.Split(price, "/")
 	} else if strings.Contains(price, "per") {
 		parsedPriceArray = strings.Split(price, "per")
+	} else {
+		parsedPriceArray = strings.Split(price, " ")
+		if len(parsedPriceArray) == 2 && !slice.ContainsString(unitTypes_DICT["each"]["each"], parsedPriceArray[1]) {
+            logger.DEBUG_WARN("%v - Failed to split price '%s' by '/' or 'per'.", index, price)
+			return "", "", "", "", false
+		}
 	}
 
 	if len(parsedPriceArray) != 2 {
-		logger.DEBUG_WARN("Failed to parse price '%s'.", price)
+		logger.DEBUG_WARN("%v - Failed to parse price '%s'.", index, price)
 		return "", "", "", "", false
 	}
 
 	regexNumeric := regexp.MustCompile(`[0-9.]+`)
 	regexNominal := regexp.MustCompile(`[^0-9.\s]+`)
+
+	logger.DEBUG("%v - Analysing string array '%v'", index, parsedPriceArray)
 
 	for measurement, measurementDict := range unitTypes_DICT {
 		for _, unitTypeSlice := range measurementDict {
@@ -112,7 +120,7 @@ func stripMeasurement(index int, logger *logger.Logger, price string) (parsedPri
 				perUnit = regexNominal.FindString(parsedPriceArray[1])
 
 				if parsedPrice == "" || perUnit == "" {
-					logger.DEBUG("parsed Price '%s' or perUnit '%s' was empty. Skipping...", parsedPrice, perUnit)
+					logger.DEBUG("%v - parsed Price '%s' or perUnit '%s' was empty. Skipping...", index, parsedPrice, perUnit)
 					continue
 				}
 				if !strings.Contains(perUnit, unit) {
@@ -123,13 +131,20 @@ func stripMeasurement(index int, logger *logger.Logger, price string) (parsedPri
 					perUnitQuantity = "1"
 				}
 
-				perUnit = unit
+				logger.DEBUG("%v - Found parsed Price '%s'", index, parsedPrice)
+				logger.DEBUG("%v - Found perUnitQuantity '%s'", index, perUnitQuantity)
+				logger.DEBUG("%v - Found perUnit '%s'", index, perUnit)
+				logger.DEBUG("%v - Found measurement '%s'", index, measurement)
+
+				// Do not uncomment this line, it causes instable unit type recognition
+				// perUnit = unit
+
 				return parsedPrice, perUnitQuantity, perUnit, measurement, true
 			}
 		}
 	}
 	
-	logger.DEBUG("perUnit '%s' did not contain any recognised Unit Type. Skipping...", perUnit)
+	logger.DEBUG("%v - perUnit '%s' did not contain any recognised Unit Type. Skipping...", index, perUnit)
 	return "", "", "unknown", "unknown", false
 }
 
@@ -137,36 +152,44 @@ func stripMeasurement(index int, logger *logger.Logger, price string) (parsedPri
 func convertPrice(index int, logger *logger.Logger, parsedPrice, perUnitQuantity, unitType string) (priceFloat float64, ok bool) {
 	parsedPriceFloat, ok := convertToFloat(index, logger, parsedPrice)
 	if !ok {
-		logger.DEBUG_WARN("Failed to convert price per unit '%s' to float", parsedPrice)
+		logger.DEBUG_WARN("%v - Failed to convert price per unit '%s' to float", index, parsedPrice)
 		return 0.0, false
 	}
 
 	parsedUnitQuantityFloat, ok := convertToFloat(index, logger, perUnitQuantity)
 	if !ok {
-		logger.DEBUG_WARN("Failed to convert unit quantity '%s' to float", perUnitQuantity)
+		logger.DEBUG_WARN("%v - Failed to convert unit quantity '%s' to float", index, perUnitQuantity)
 		return 0.0, false
 	}
 
-	isKiloOrLitre := slice.ContainsString(unitTypes_DICT["kilogram"]["kilogram"], unitType) || slice.ContainsString(unitTypes_DICT["litre"]["litre"], unitType)
-	isGramOrMillilitre := slice.ContainsString(unitTypes_DICT["kilogram"]["gram"], unitType) || slice.ContainsString(unitTypes_DICT["litre"]["millilitre"], unitType)
-	isCentilitre := slice.ContainsString(unitTypes_DICT["litre"]["centilitre"], unitType)
-	isEach := slice.ContainsString(unitTypes_DICT["each"]["each"], unitType)
+	isCentilitre := (slice.ContainsString(unitTypes_DICT["litre"]["centilitre"], unitType))
+	logger.DEBUG("%v - isCentilitre: %v", index, isCentilitre)
 
-	if isKiloOrLitre {
-		return parsedPriceFloat, true
+	isGramOrMillilitre := (slice.ContainsString(unitTypes_DICT["kilogram"]["gram"], unitType) || slice.ContainsString(unitTypes_DICT["litre"]["millilitre"], unitType))
+	logger.DEBUG("%v - isGramOrMillilitre: %v", index, isGramOrMillilitre)
+
+	isKiloOrLitre := (slice.ContainsString(unitTypes_DICT["kilogram"]["kilogram"], unitType) || slice.ContainsString(unitTypes_DICT["litre"]["litre"], unitType))
+	logger.DEBUG("%v - isKiloOrLitre: %v", index, isKiloOrLitre)
+	
+	isEach := slice.ContainsString(unitTypes_DICT["each"]["each"], unitType)
+	logger.DEBUG("%v - isEach: %v", index, isEach)
+
+	
+	if isCentilitre {
+		return (parsedPriceFloat * (100 / parsedUnitQuantityFloat)), true
 
 	} else if isGramOrMillilitre {
 		return (parsedPriceFloat * (1000 / parsedUnitQuantityFloat)), true
 
-	} else if isCentilitre {
-		return (parsedPriceFloat * (100 / parsedUnitQuantityFloat)), true
+	} else if isKiloOrLitre {
+		return parsedPriceFloat, true
 
 	} else if isEach {
 		return (parsedPriceFloat * parsedUnitQuantityFloat), true
 	}
 
 
-	logger.DEBUG_WARN("Failed to convert price per unit type '%s'", parsedPrice)
+	logger.DEBUG_WARN("%v - Failed to convert price per unit type '%s'", index, parsedPrice)
 	return 0.0, false
 }
 
