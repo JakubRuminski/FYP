086e1cc1599cf15f0e98bb7973f8dd9ea05e3d6e
Jakub Ruminski <jakubruminski09@gmail.com>
2024-02-22 08:44:18 +0000

    mux.go:  + Added MAX_REQUESTS functionality so only a restricted amount of requests can be handled at any one time.

--- go/router/mux/mux.go
+++ go/router/mux/mux.go
@@ -9,6 +9,31 @@ import (
 )
 
 
+var MAX_REQUESTS int
+
+// This function is a middleware that limits the number of requests
+// that can be handled at the same time.
+//
+// If called with a semaphore of size 1, it will only allow one request to be handled at a time.
+// 
+func RequestLimiter(logger *logger.Logger, next http.HandlerFunc) http.HandlerFunc {
+	semaphore := make(chan struct{}, MAX_REQUESTS)
+	logger.DEBUG("Created request limiter")
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		semaphore <- struct{}{}
+		logger.INFO("Acquired semaphore")
+		
+		defer func() { 
+			logger.INFO("Released semaphore")
+			<-semaphore
+		}()
+
+		next(w, r)
+		logger.DEBUG("Request handled")
+	}
+}
+
 func INIT(logger *logger.Logger) (port string, mux *http.ServeMux, ok bool) {
 	mux = http.NewServeMux()
 
@@ -16,14 +41,17 @@ func INIT(logger *logger.Logger) (port string, mux *http.ServeMux, ok bool) {
 	port = "PORT"
 	ok = env.GetKeys(logger, &environment, &port)
 	if !ok { return "", nil, false }
+
+	MAX_REQUESTS, ok = env.GetInt( logger, "MAX_REQUESTS" )
+    if !ok { return "", nil, false }
 	
 	logger.SetEnvironment(environment)
 
-	mux.HandleFunc("/", logger.Middleware(request.HandleRequest))
-	mux.HandleFunc("/static/", logger.Middleware(request.HandleRequest))
+	mux.HandleFunc("/", RequestLimiter( logger, logger.Middleware(request.HandleRequest) ))
+	mux.HandleFunc("/static/", RequestLimiter( logger, logger.Middleware(request.HandleRequest) ))
 
-	mux.HandleFunc("/api/search", logger.Middleware(request.HandleApiRequest))
-	mux.HandleFunc("/api/add_item", logger.Middleware(request.HandleApiRequest))
+	mux.HandleFunc("/api/search", RequestLimiter( logger, logger.Middleware(request.HandleApiRequest) ))
+	mux.HandleFunc("/api/add_item", RequestLimiter( logger, logger.Middleware(request.HandleApiRequest) ))
 
 	return port, mux, true
 }
