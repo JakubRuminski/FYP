c5416cd2fa96aae3fe919fd7963c97b028784b7c
Jakub Ruminski <jakubruminski09@gmail.com>
2024-04-07 17:35:24 +0100

    query_searchs.go:  + Added `expiry` to 'searches' table

--- go/api/query/query_searchs/query_searchs.go
+++ go/api/query/query_searchs/query_searchs.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/jakubruminski/FYP/go/api/product"
+	"github.com/jakubruminski/FYP/go/utils/env"
 	"github.com/jakubruminski/FYP/go/utils/logger"
 	"github.com/jakubruminski/FYP/go/utils/postgres"
 )
@@ -19,6 +20,7 @@ type SearchTerm struct {
 
 	FetchCount          int        `json:"fetch_count"`
 	LastFetch           int        `json:"last_fetch"`
+	Expiry 			    int        `json:"expiry"`
 }
 
 func INIT(logger *logger.Logger) (ok bool) {
@@ -29,7 +31,8 @@ func INIT(logger *logger.Logger) (ok bool) {
 			search_term      VARCHAR(255),
 			product_id       INT,
 			fetch_count      INT,
-			last_fetch       INT
+			last_fetch       INT,
+			expiry           INT
 		)
 	`
 
@@ -42,13 +45,13 @@ func INIT(logger *logger.Logger) (ok bool) {
 	return true	
 }
 
-func Get(logger *logger.Logger, tx *sql.Tx, searchTerm string) (productIDs *[]*string, ok bool) {
+func GetIDs(logger *logger.Logger, tx *sql.Tx, searchTerm string) (productIDs *[]*int64, ok bool) {
 	searchTerm = strings.ToLower(searchTerm)
 
 	query := `SELECT product_id FROM searches WHERE search_term = $1`
 
-	productIDs = &[]*string{}
-	ok = postgres.ExecuteContextLookUpQuery(logger, tx, get, query, searchTerm, productIDs)
+	productIDs = &[]*int64{}
+	ok = postgres.ExecuteContextLookUpQuery(logger, tx, getIDs, query, searchTerm, productIDs)
 	if !ok {
 		logger.ERROR("Failed to get product IDs")
 		return nil, false
@@ -59,7 +62,7 @@ func Get(logger *logger.Logger, tx *sql.Tx, searchTerm string) (productIDs *[]*s
 }
 
 
-func get(logger *logger.Logger, tx *sql.Tx, ctx context.Context, query string, args ...interface{}) (ok bool) {
+func getIDs(logger *logger.Logger, tx *sql.Tx, ctx context.Context, query string, args ...interface{}) (ok bool) {
 
 	searchTerm, ok := args[0].(string)
 	if !ok {
@@ -73,20 +76,58 @@ func get(logger *logger.Logger, tx *sql.Tx, ctx context.Context, query string, a
 		return false
 	}
 
-	productIDs, ok := args[1].(*[]*string)
+	productIDs, ok := args[1].(*[]*int64)
 	if !ok {
 		logger.ERROR("Failed to get product IDs")
 		return false
 	}
 
 	for rows.Next() {
-		var productID string
-		err := rows.Scan(&productID)
+		productID := new(int64)
+		err := rows.Scan(productID)
 		if err != nil {
 			logger.ERROR("Failed to scan product ID")
 			return false
 		}
-		*productIDs = append(*productIDs, &productID)
+		*productIDs = append(*productIDs, productID)
+	}
+	return true
+}
+
+
+func GetExpiry(logger *logger.Logger, tx *sql.Tx, searchTerm string) (expiry int, ok bool) {
+	searchTerm = strings.ToLower(searchTerm)
+
+	query := `SELECT last_fetch FROM searches WHERE search_term = $1`
+
+	ok = postgres.ExecuteContextLookUpQuery(logger, tx, getExpiry, query, searchTerm, &expiry)
+	if !ok {
+		logger.ERROR("Failed to get expiry")
+		return 0, false
+	}
+
+	return expiry, true
+}
+
+
+func getExpiry(logger *logger.Logger, tx *sql.Tx, ctx context.Context, query string, args ...interface{}) (ok bool) {
+	
+	searchTerm, ok := args[0].(string)
+	if !ok {
+		logger.ERROR("Failed to get search term")
+		return false
+	}
+
+	expiry, ok := args[1].(*int)
+	if !ok {
+		logger.ERROR("Failed to get expiry")
+		return false
+	}
+
+	err := tx.QueryRowContext(ctx, query, searchTerm).Scan(expiry)
+	if err != nil {
+		logger.ERROR("Failed to execute the query. Reason: %s", err)
+		return false
 	}
 	return true
 }
@@ -94,12 +135,19 @@ func get(logger *logger.Logger, tx *sql.Tx, ctx context.Context, query string, a
 
 func Add(logger *logger.Logger, tx *sql.Tx, searchTerm string, products *[]*product.Product) (ok bool) {
 
+	expiry, ok := env.GetInt(logger, "SEARCH_EXPIRY_IN_DAYS")
+	if !ok {
+		logger.ERROR("Failed to get search expiry")
+		return false
+	}
 	searchTerm = strings.ToLower(searchTerm)
 	
-	lastFetched := int(time.Now().Unix())
-	query := `INSERT INTO searches (search_term, product_id, fetch_count, last_fetch) VALUES ($1, $2, 1, $3)`
+	now := int(time.Now().Unix()) 
+	lastFetched := now
+	expiry = now + (expiry * 24 * 60 * 60)
+	query := `INSERT INTO searches (search_term, product_id, fetch_count, last_fetch, expiry) VALUES ($1, $2, 1, $3, $4)`
 
-	ok = postgres.ExecuteContextChangeQuery(logger, tx, add, query, searchTerm, products, lastFetched)
+	ok = postgres.ExecuteContextChangeQuery(logger, tx, add, query, searchTerm, products, lastFetched, expiry)
 	if !ok {
 		logger.ERROR("Failed to add search term")
 		return false
@@ -129,8 +177,14 @@ func add(logger *logger.Logger, tx *sql.Tx, ctx context.Context, query string, a
 		return false
 	}
 
+	expiry, ok := args[3].(int)
+	if !ok {
+		logger.ERROR("Failed to get expiry")
+		return false
+	}
+
 	for _, product := range *products {
-		_, err := tx.ExecContext(ctx, query, searchTerm, product.ID, lastFetched)
+		_, err := tx.ExecContext(ctx, query, searchTerm, product.ID, lastFetched, expiry)
 		if err != nil {
 			logger.ERROR("Failed to execute the query. Reason: %s", err)
 			return false
